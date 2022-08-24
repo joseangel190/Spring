a62132392d6a546284659031d603a279c22b60c4
Jose Angel Guerrero <joseangel.guerrero1926@gmail.com>
2022-08-23 19:40:56 -0500

    microservice

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+    @Column(name = "lastname", length = 50, nullable = false)
+    private String lastname;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
